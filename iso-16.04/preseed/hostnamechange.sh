9351ff5bfd061fa20c91a50c40adcc852ff5c587
Rodel Manalo Talampas <rodel-talampas@users.noreply.github.com>
2018-04-10 11:52:52 +1000

    Update hostnamechange.sh

--- iso-16.04/preseed/hostnamechange.sh
+++ iso-16.04/preseed/hostnamechange.sh
@@ -7,6 +7,6 @@ sed -i '/ubuntu/d' /etc/hosts
 
 v1=`ip route get 8.8.8.8 | awk '{print $NF; exit}'`
 v1=`echo "${v1//./-}"`
-v2=LockerNetwork-$v1
+v2=CustomJavaFx-$v1
 echo $v2 > /etc/hostname #change the hostname with ip suffix 
 echo 127.0.1.1  $v2 >> /etc/hosts #add in new host name in etc host file 
